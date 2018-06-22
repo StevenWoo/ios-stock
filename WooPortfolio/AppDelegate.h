5c3adee7008bb1da6b5cbea0ee68130c3ba6f28f
Steven Woo <stevenwoo@gmail.com>
2018-06-21 19:28:32 -0700

    Initial Commit

---
+++ WooPortfolio/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WooPortfolio
+//
+//  Created by Steven Woo on 6/21/18.
+//  Copyright © 2018 swoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WooPortfolio/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WooPortfolio
+//
+//  Created by Steven Woo on 6/21/18.
+//  Copyright © 2018 swoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
